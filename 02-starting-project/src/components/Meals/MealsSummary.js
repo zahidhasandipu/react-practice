ec5ad4965e3a3a9ae1fd47252b58121fa899db58
zahidhasandipu <zahidhasandipu@gmail.com>
2021-07-29 23:24:37 -0700

    Done: Adding Http & Forms To The Food Order App

--- 02-starting-project/src/components/Meals/MealsSummary.js
+++ 02-starting-project/src/components/Meals/MealsSummary.js
@@ -1,19 +1,18 @@
 import classes from './MealsSummary.module.css';
 
 const MealsSummary = () => {
-  return (
-    <section className={classes.summary}>
-      <h2>Delicious Food, Delivered To You</h2>
-      <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
-      </p>
-      <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
-      </p>
-    </section>
-  );
+    return (
+        <section className={classes.summary}>
+     <h2> Feeling Hungry? Get It Delivered Right Away</h2>
+     <p>
+     Choose your favorite food from 3000+ restaurants and enjoy at your home.
+     </p>
+     <p>
+     Craving for something delecious today? Get it delivered to your doorsteps or pick up whichever is convenient for you. As always there is no fee for pickup orders. Delivery within 40 minutes.
+     </p>
+     </section>
+    );
+
 };
 
 export default MealsSummary;
